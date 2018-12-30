eeccf5d85746321a75bd7ea9367d5c8c4d9c774f
ssekito_charles <charles.ssekito@andela.com>
2018-12-30 17:09:02 +0300

    add styles folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./store";
-import "./style.scss";
+import "./styles/style.scss";
 import App from "./components/App";
 
 	ReactDOM.render(
