acb240d6cd8fe4521ffb1d42847a13d6f3ed255a
Ssekitto charlse <bozicslxye1@gmail.com>
2018-12-30 18:05:07 +0300

    [ch #162880379] add react router to app (#2)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import { connect } from "react-redux";
+import {BrowserRouter, Route,Switch } from 'react-router-dom';
 
 import { appAction } from "../actions/appAction";
-const App = () => (<p>Welcome to this react project</p>);
+
+
+export const home = () => {
+	return (
+		<div>
+			<p>Welcome to the home component</p>
+		</div>
+	);
+}
+
+
+export const notFound = () => {
+	return (
+		<div>
+			<p>The page is not Found 404</p>
+		</div>
+	);
+}
+
+const App = () => {
+	return(
+	<BrowserRouter>
+	<Switch>
+		<Route path="/" component={home} exact/>
+		<Route component={notFound} />
+	</Switch>
+	</BrowserRouter>);
+	}
 
 export default App;
